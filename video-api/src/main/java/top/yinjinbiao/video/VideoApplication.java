3574c0c684412471f52bad8e617ad10aeb539f43
yinjinbiao <731509863@qq.com>
2020-04-01 17:43:29 +0800

    flowable 依赖引入

--- video-api/src/main/java/top/yinjinbiao/video/VideoApplication.java
+++ video-api/src/main/java/top/yinjinbiao/video/VideoApplication.java
@@ -7,7 +7,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
 @SpringBootApplication
-@MapperScan(basePackages = {"top.yinjinbiao.video.admin.mapper","top.yinjinbiao.video.upload.mapper", "top.yinjinbiao.video.biz.article.mapper"})
+@MapperScan(basePackages = {"org.flowable.ui.modeler","org.flowable.ui.common","top.yinjinbiao.video.admin.mapper","top.yinjinbiao.video.upload.mapper", "top.yinjinbiao.video.biz.article.mapper"})
 class VideoApplication {
     public static void main(String[] args) {
         SpringApplication.run(VideoApplication.class,args);
